2bbb22531609ef01b5eb84597570cf05d5bbc06c
Vincent Vankrunkelsven <vincent@datacamp.com>
2016-05-22 16:44:33 +0200

    Update test_while_loop.py

--- pythonwhat/test_while_loop.py
+++ pythonwhat/test_while_loop.py
@@ -9,47 +9,52 @@ def test_while_loop(index=1,
                     body=None,
                     orelse=None,
                     expand_message=True):
-    """Test parts of the for loop.
+    """Test parts of the while loop.
 
-    This test function will allow you to extract parts of a specific for loop and perform a set of tests
-    specifically on these parts. A for loop consists of two parts: the sequence, `for_iter`, which is the
-    values over which are looped, and the `body`. A for loop can have a else part as well, `orelse`, but
-    this is almost never used.
+    This test function will allow you to extract parts of a specific while loop and perform a set of tests
+    specifically on these parts. A while loop generally consists of two parts: the condition test, `test`, 
+    which is the condition that is tested each loop, and the `body`. A for while can have a else part as well, 
+    `orelse`, but this is almost never used.
+        ``a = 10``
+        ``while a < 5:``
+        ``    print(a)``
+        ``    a -= 1``
 
-        ``for i in range(10):``
-        ``    print(i)``
-
-    Has `range(10)` as the sequence and `print(i)` as the body.
+    Has `a < 5` as the condition test and `print(i)` as the body.
 
     Example:
       student_code
-        | ``for i in range(10):``
-        | ``    print(i)``
+        | ``a = 10``
+        | ``while a < 5:``
+        | ``    print(a)``
+        | ``    a -= 1``
       solution_code
-        | ``for n in range(10):``
-        | ``    print(n)``
+        | ``a = 20``
+        | ``while a < 5:``
+        | ``    print(a)``
+        | ``    a -= 1``
       sct
-        | ``test_for_loop(1,``
-        | ``              for_iter = lamdba: test_function("range"),``
-        | ``              body = lambda: test_expression_output(context_val = [5])``
-      This SCT will evaluate to TRUE as the function `"range"` is used in the sequence and the function
-      `test_exression_output()` will pass on the body code.
+        | ``test_while_loop(1,``
+        | ``                test = lamdba: test_expression_result({"a": 5}),``
+        | ``                body = lambda: test_expression_output({"a": 5}))``
+      This SCT will evaluate to True as condition test will have thes same result in student
+      and solution code and `test_exression_output()` will pass on the body code.
 
     Args:
       index (int): index of the function call to be checked. Defaults to 1.
-      for_iter: this argument holds the part of code that will be ran to check the sequence of the for loop.
+      test: this argument holds the part of code that will be ran to check the condition test of the while loop.
         It should be passed as a lambda expression or a function definition. The functions that are ran should
-        be other pythonwhat test functions, and they will be tested specifically on only the sequence part of
-        the for loop.
-      body: this argument holds the part of code that will be ran to check the body of the for loop.
+        be other pythonwhat test functions, and they will be tested specifically on only the condition test of
+        the while loop.
+      body: this argument holds the part of code that will be ran to check the body of the while loop.
         It should be passed as a lambda expression or a function definition. The functions that are ran should
         be other pythonwhat test functions, and they will be tested specifically on only the body of
-        the for loop.
-      orelse: this argument holds the part of code that will be ran to check the else part of the for loop.
+        the while loop.
+      orelse: this argument holds the part of code that will be ran to check the else part of the while loop.
         It should be passed as a lambda expression or a function definition. The functions that are ran should
         be other pythonwhat test functions, and they will be tested specifically on only the else part of
-        the for loop.
-      expand_message (bool): if true, feedback messages will be expanded with `in the ___ of the for loop on
+        the while loop.
+      expand_message (bool): if true, feedback messages will be expanded with `in the ___ of the while loop on
         line ___`. Defaults to True. If False, `test_for_loop()` will generate no extra feedback.
     """
     state = State.active_state
