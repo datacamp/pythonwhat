1a7e9310890aafdebfc8558de9cfa6604af2cce4
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-07-01 21:16:37 -0700

    make tests more granular

--- tests/test_test_object_accessed.py
+++ tests/test_test_object_accessed.py
@@ -22,47 +22,52 @@ print(arr.data)
       '''      
     }
 
-  def test_objectOnly(self):
+  def test_objectArr(self):
     self.data["DC_SCT"] = 'test_object_accessed("arr")'
     sct_payload = helper.run(self.data)
     self.assertEqual(sct_payload['correct'], True)
 
+  def test_objectArrTwice(self):
     self.data["DC_SCT"] = 'test_object_accessed("arr", times=2)'
     sct_payload = helper.run(self.data)
     self.assertEqual(sct_payload['correct'], True)
 
+  def test_objectArrThrice(self):
     self.data["DC_SCT"] = 'test_object_accessed("arr", times=3)'
     sct_payload = helper.run(self.data)
     self.assertEqual(sct_payload['correct'], False)
     self.assertEqual(sct_payload['message'], "Have you accessed <code>arr</code> at least 3 times?")
 
+  def test_objectArrThriceCustom(self):
     self.data["DC_SCT"] = 'test_object_accessed("arr", times=3, not_accessed_msg="silly")'
     sct_payload = helper.run(self.data)
     self.assertEqual(sct_payload['correct'], False)
     self.assertEqual(sct_payload['message'], "silly")
 
-
   def test_objectAndAttribute(self):
-
     self.data["DC_SCT"] = 'test_object_accessed("arr.shape")'
     sct_payload = helper.run(self.data)
     self.assertEqual(sct_payload['correct'], True)
 
+  def test_objectAndAttributeTwice(self):
     self.data["DC_SCT"] = 'test_object_accessed("arr.shape", times=2)'
     sct_payload = helper.run(self.data)
     self.assertEqual(sct_payload['correct'], False)
     self.assertEqual(sct_payload['message'], "Have you accessed <code>arr.shape</code> at least twice?")
 
+  def test_objectAndAttributeTwiceCustom(self):
     self.data["DC_SCT"] = 'test_object_accessed("arr.shape", times=2, not_accessed_msg="silly")'
     sct_payload = helper.run(self.data)
     self.assertEqual(sct_payload['correct'], False)
     self.assertEqual(sct_payload['message'], "silly")
     
+  def test_objectAndAttributeOnce(self):
     self.data["DC_SCT"] = 'test_object_accessed("arr.dtype")'
     sct_payload = helper.run(self.data)
     self.assertEqual(sct_payload['correct'], False)
     self.assertEqual(sct_payload['message'], "Have you accessed <code>arr.dtype</code>?")
 
+  def test_objectAndAttributeOnceCustom(self):
     self.data["DC_SCT"] = 'test_object_accessed("arr.dtype", not_accessed_msg="silly")'
     sct_payload = helper.run(self.data)
     self.assertEqual(sct_payload['correct'], False)
