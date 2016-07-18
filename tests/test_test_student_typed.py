1c924da84776a978b28f78c9f3915227758f2d87
Filip Schouwenaars <filip.schouwenaars@gmail.com>
2016-07-18 09:26:18 +0200

    add tests for wiki test_studnet_typed

--- tests/test_test_student_typed.py
+++ tests/test_test_student_typed.py
@@ -37,5 +37,39 @@ success_msg("You typed the correct comment.")
         self.assertFalse(sct_payload['correct'])
         self.assertEqual(sct_payload['message'], "Make sure to add the instructed comment before <code>print(7+10)</code>.")
 
+class TestWikiExample(unittest.TestCase):
+
+    def test_wikiexample1(self):
+        self.data = {
+            "DC_PEC": '',
+            "DC_SOLUTION": 's = sum(range(10))\nprint(s)',
+            "DC_SCT": 'test_student_typed("sum(range(", pattern = False)',
+            "DC_CODE": 's = sum(range(10))\nprint(s)'
+        }
+        sct_payload = helper.run(self.data)
+        self.assertTrue(sct_payload['correct'])
+
+    def test_wikiexample2(self):
+        self.data = {
+            "DC_PEC": '',
+            "DC_SOLUTION": 's = sum(range(10))\nprint(s)',
+            "DC_SCT": 'test_student_typed("sum\s*\(\s*range\s*\(")',
+            "DC_CODE": 's = sum(range(10))\nprint(s)'
+        }
+        sct_payload = helper.run(self.data)
+        print(sct_payload)
+        self.assertTrue(sct_payload['correct'])
+
+    def test_wikiexample1(self):
+        self.data = {
+            "DC_PEC": '',
+            "DC_SOLUTION": 's = sum(range(10))\nprint(s)',
+            "DC_SCT": 'test_student_typed("sum\s+\(\s*range\s*\(")',
+            "DC_CODE": 's = sum(range(10))\nprint(s)'
+        }
+        sct_payload = helper.run(self.data)
+        print(sct_payload)
+        self.assertFalse(sct_payload['correct'])
+
 if __name__ == "__main__":
     unittest.main()
