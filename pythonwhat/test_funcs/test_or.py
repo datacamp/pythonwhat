7d80d49ef1f30648bf9fd798d0941b775d022c64
Michael Chow <machow@princeton.edu>
2016-11-11 21:57:36 -0500

    test_or uses first feedback rather than just its message

--- pythonwhat/test_funcs/test_or.py
+++ pythonwhat/test_funcs/test_or.py
@@ -6,25 +6,19 @@ def test_or(*tests, state=None):
     rep = Reporter.active_reporter
     rep.set_tag("fun", "test_or")
 
-
-
     success = False
-    first_message = None
-
-
-    success = False
-    first_message = None
+    first_feedback = None
     for test in tests: 
         try: 
             multi(test, state=state)
             success = True
         except TestFail as e:
-            if not first_message: first_message = rep.feedback.message 
+            if not first_feedback: first_feedback = rep.feedback
             rep.failed_test = False
 
         if success: 
             return
     
     rep.failed_test = True
-    rep.feedback.message = first_message
+    rep.feedback = first_feedback
     raise TestFail
