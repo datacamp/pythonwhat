295db1d5842fb1b03a005947ce35971eb37a90e8
Filip Schouwenaars <filip@datacamp.com>
2017-01-03 13:51:06 +0100

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 	name='pythonwhat',
-	version='2.1.1',
+	version='2.1.2',
 	packages=['pythonwhat', 'pythonwhat.test_funcs'],
 	install_requires=["dill", "IPython", "numpy", "pandas", "markdown2"]
 )
