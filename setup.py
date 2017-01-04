c4ae606f64fe64428986a228e9a514369881b46b
Michael Chow <machow@princeton.edu>
2017-01-04 16:10:13 -0500

    version 2.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 	name='pythonwhat',
-	version='2.1.2',
+	version='2.2.0',
 	packages=['pythonwhat', 'pythonwhat.test_funcs'],
 	install_requires=["dill", "IPython", "numpy", "pandas", "markdown2"]
 )
