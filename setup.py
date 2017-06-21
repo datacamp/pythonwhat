2a95906dff60eb90d07e5d07583721478ce07640
Michael Chow <machow@princeton.edu>
2017-06-21 12:49:14 -0400

    fix typo in setup.py, referring to sqlwhat...

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('sqlwhat/__init__.py', 'rb') as f:
+with open('pythonwhat/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
