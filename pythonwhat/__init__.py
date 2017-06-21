6805f36881e8f02c4c2258c4a98f2877761ded20
Michael Chow <machow@princeton.edu>
2017-06-21 12:44:43 -0400

    use setuptools for deployment, put version in __init__.py

--- pythonwhat/__init__.py
+++ pythonwhat/__init__.py
@@ -1,2 +1,4 @@
+__version__ = '2.7.0'
+
 from .test_exercise import test_exercise, allow_errors
 
--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import re
+import ast
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('sqlwhat/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
 
 setup(
 	name='pythonwhat',
-	version='2.7.0',
+	version=version,
 	packages=['pythonwhat', 'pythonwhat.test_funcs'],
-	install_requires=["dill", "IPython", "numpy", "pandas", "markdown2", "jinja2"]
-)
+	install_requires=["dill", "IPython", "numpy", "pandas", "markdown2", "jinja2"],
+        maintainer = 'Michael Chow',
+        maintainer_email = 'michael@datacamp.com',
+        url = 'https://github.com/datacamp/pythonwhat')
