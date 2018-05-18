0763fb85a381bfc5fb0687c1c746693dae5ee0b9
Filip Schouwenaars <filip@datacamp.com>
2018-05-18 09:11:57 +0200

    README in setup.py long_description

--- setup.py
+++ setup.py
@@ -2,19 +2,27 @@
 
 import re
 import ast
+from os import path
 from setuptools import setup
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('pythonwhat/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+PACKAGE_NAME = 'pythonwhat'
+HERE = path.abspath(path.dirname(__file__))
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as fp:
+    README = fp.read()
+with open(path.join(HERE, PACKAGE_NAME, '__init__.py'), 'rb') as fp:
+    VERSION = str(ast.literal_eval(_version_re.search(
+        fp.read().decode('utf-8')).group(1)))
 
-setup(
-        name='pythonwhat',
-        version=version,
-        packages=['pythonwhat', 'pythonwhat.test_funcs'],
-        install_requires=["dill", "numpy", "pandas", "markdown2", "jinja2", "asttokens"],
-        maintainer = 'Michael Chow',
-        maintainer_email = 'michael@datacamp.com',
-        url = 'https://github.com/datacamp/pythonwhat')
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      packages=[PACKAGE_NAME, 'pythonwhat.test_funcs'],
+      install_requires=["dill", "numpy", "pandas", "markdown2", "jinja2", "asttokens"],
+      long_description=README,
+      license='GNU version 3',
+      author='DataCamp',
+      author_email='content-engineering@datacamp.com',
+      maintainer='Filip Schouwenaars',
+      maintainer_email='filip@datacamp.com',
+      url='https://github.com/datacamp/pythonwhat')
