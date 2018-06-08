188345c9781278db5498557ff3027bb9d1d204f9
Filip Schouwenaars <filip@datacamp.com>
2018-06-08 10:24:11 +0200

    fix to post-execution error message: just point to console

--- tests/test_test_exercise.py
+++ tests/test_test_exercise.py
@@ -1,61 +1,68 @@
-import unittest
+import pytest
 import helper
 
-class TestTestExerciseError(unittest.TestCase):
+def test_normal_pass():
+	data = {
+		"DC_PEC": '#no pec',
+		"DC_CODE": 'x = 4',
+		"DC_SOLUTION": 'x = 4',
+		"DC_SCT": 'test_object("x")\nsuccess_msg("nice")'
+	}
+	output = helper.run(data)
+	assert output['correct']
+	assert output['message'] == 'nice'
 
-	def test_normal_pass(self):
-		self.data = {
-			"DC_PEC": '#no pec',
-			"DC_CODE": 'x = 4',
-			"DC_SOLUTION": 'x = 4',
-			"DC_SCT": 'test_object("x")\nsuccess_msg("nice")'
-		}
-		output = helper.run(self.data)
-		self.assertTrue(output['correct'])
-		self.assertEqual(output['message'], 'nice')
+def test_normal_fail():
+	data = {
+		"DC_PEC": '#no pec',
+		"DC_CODE": 'x = 4',
+		"DC_SOLUTION": 'x = 6',
+		"DC_SCT": 'test_object("x")\nsuccess_msg("nice")'
+	}
+	output = helper.run(data)
+	assert not output['correct']
 
-	def test_normal_fail(self):
-		self.data = {
-			"DC_PEC": '#no pec',
-			"DC_CODE": 'x = 4',
-			"DC_SOLUTION": 'x = 6',
-			"DC_SCT": 'test_object("x")\nsuccess_msg("nice")'
-		}
-		output = helper.run(self.data)
-		self.assertFalse(output['correct'])
+@pytest.mark.debug
+def test_normal_error():
+	data = {
+		"DC_PEC": '#no pec',
+		"DC_CODE": 'x = y',
+		"DC_SOLUTION": 'x = 6',
+		"DC_SCT": '# no sct'
+	}
+	output = helper.run(data)
+	assert not output['correct']
+	assert 'your code contains an error.' in output['message']
 
-	def test_syntax_error(self):
-		self.data = {
-			"DC_PEC": '# no pec',
-			"DC_CODE": 'print "yolo"',
-			"DC_SOLUTION": 'x = 6',
-			"DC_SCT": 'test_object("x")'
-		}
-		output = helper.run(self.data)
-		self.assertFalse(output['correct'])
-		self.assertEqual(output['message'], "Your code can not be executed due to a syntax error:<br><code>Missing parentheses in call to 'print' (script.py, line 1).</code>") 
+def test_syntax_error():
+	data = {
+		"DC_PEC": '# no pec',
+		"DC_CODE": 'print "yolo"',
+		"DC_SOLUTION": 'x = 6',
+		"DC_SCT": 'test_object("x")'
+	}
+	output = helper.run(data)
+	assert not output['correct']
+	assert "Your code can not be executed due to a syntax error" in output['message']
 
-	def test_indentation_error(self):
-		self.data = {
-			"DC_PEC": '# no pec',
-			"DC_CODE": '	print("yolo")',
-			"DC_SOLUTION": 'x = 6',
-			"DC_SCT": 'test_object("x")'
-		}
-		output = helper.run(self.data)
-		self.assertFalse(output['correct'])
-		self.assertEqual(output['message'], "Your code could not be parsed due to an error in the indentation:<br><code>unexpected indent (script.py, line 1).</code>")
+def test_indentation_error():
+	data = {
+		"DC_PEC": '# no pec',
+		"DC_CODE": '	print("yolo")',
+		"DC_SOLUTION": 'x = 6',
+		"DC_SCT": 'test_object("x")'
+	}
+	output = helper.run(data)
+	assert not output['correct']
+	assert "Your code could not be parsed due to an error in the indentation" in output['message']
 
-	def test_enrichment_error(self):
-		self.data = {
-			"DC_PEC": '# no pec',
-			"DC_CODE": '',
-			"DC_SOLUTION": 'x = 6',
-			"DC_SCT": 'test_object("x")'
-		}
-		output = helper.run(self.data)
-		self.assertFalse(output['correct'])
-		helper.test_absent_lines(self, output)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_enrichment_error():
+	data = {
+		"DC_PEC": '# no pec',
+		"DC_CODE": '',
+		"DC_SOLUTION": 'x = 6',
+		"DC_SCT": 'test_object("x")'
+	}
+	output = helper.run(data)
+	assert not output['correct']
+	assert not 'line_start' in output
