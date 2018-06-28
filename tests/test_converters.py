5d6f1bc400bc259b3e49e3447ee8ea9c145e6c76
Filip Schouwenaars <filip@datacamp.com>
2018-06-28 10:49:42 +0200

    Resolve part 1 of #296

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -18,7 +18,7 @@ def test_dict_keys():
         "DC_PEC": "",
         "DC_SOLUTION": "x = {'a': 1, 'b': 2}; print(x.keys())",
         "DC_CODE": "x = {'b':2, 'a': 1}; print(x.keys())",
-        "DC_SCT": "test_function_v2('print', params = ['value'])"
+        "DC_SCT": "Ex().check_function('print').check_args('value').has_equal_value()"
     }
     sct_payload = helper.run(data)
     assert sct_payload['correct']
@@ -29,7 +29,7 @@ def test_dictitems():
         "DC_PEC": "",
         "DC_SOLUTION": "x = {'france':'paris', 'spain':'madrid'}.items()",
         "DC_CODE": "x = {'spain':'madrid', 'france':'paris'}.items()",
-        "DC_SCT": "test_object('x')"
+        "DC_SCT": "Ex().check_object('x').has_equal_value()"
     }
     sct_payload = helper.run(data)
     assert sct_payload['correct']
@@ -40,7 +40,7 @@ def test_beautiful_soup():
         "DC_PEC": "import requests; from bs4 import BeautifulSoup",
         "DC_SOLUTION": "soup = BeautifulSoup(requests.get('https://www.python.org/~guido/').text, 'html5lib'); print(soup.title); a_tags = soup.find_all('a')",
         "DC_CODE": "soup = BeautifulSoup(requests.get('https://www.python.org/~guido/').text, 'html5lib'); print(soup.title); a_tags = soup.find_all('a')",
-        "DC_SCT": "test_object('soup'); test_function_v2('print', params = ['value']); test_object('a_tags')"
+        "DC_SCT": "Ex().check_object('soup').has_equal_value(); Ex().check_function('print').check_args('value').has_equal_value(); Ex().check_object('a_tags').has_equal_value()"
     }
     sct_payload = helper.run(data)
     assert sct_payload['correct']
@@ -51,7 +51,7 @@ def test_hdf5():
         "DC_PEC": "import numpy as np; import h5py; file = 'LIGO_data.hdf5'; from urllib.request import urlretrieve; urlretrieve('https://s3.amazonaws.com/assets.datacamp.com/production/course_998/datasets/L-L1_LOSC_4_V1-1126259446-32.hdf5', 'LIGO_data.hdf5')",
         "DC_SOLUTION": "data = h5py.File(file, 'r'); group = data['strain']",
         "DC_CODE": "data = h5py.File(file, 'r'); group = data['strain']",
-        "DC_SCT": "test_object('file'); test_object('data'); test_object('group')"
+        "DC_SCT": "Ex().check_object('file').has_equal_value(); Ex().check_object('data').has_equal_value(); Ex().check_object('group').has_equal_value()"
     }
     sct_payload = helper.run(data)
     assert sct_payload['correct']
