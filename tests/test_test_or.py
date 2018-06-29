b2b9a3be110bb43455113578cb708f62cbf917fb
Filip Schouwenaars <filip@datacamp.com>
2018-06-29 13:19:24 +0200

    better indentation

--- tests/test_test_or.py
+++ tests/test_test_or.py
@@ -55,9 +55,9 @@ class TestExercise1(unittest.TestCase):
 class TestNestedOr(unittest.TestCase):
     def setUp(self):
         self.data = {
-                "DC_SOLUTION": """for ii in range(10): print('yes') if ii < 2 else print('no')""",
-                "DC_SCT": """test_for_loop(1, body=test_if_exp(body=test_or(test_student_typed('print'))))"""
-                }
+            "DC_SOLUTION": """for ii in range(10): print('yes') if ii < 2 else print('no')""",
+            "DC_SCT": """test_for_loop(1, body=test_if_exp(body=test_or(test_student_typed('print'))))"""
+        }
 
     def test_pass(self):
         self.data["DC_CODE"] = self.data["DC_SOLUTION"]
