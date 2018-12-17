b360fde2d7167fe8e1d8c68f92e2ddf88e8ad7d3
Jeroen Hermans <jeroen@datacamp.com>
2018-12-17 20:26:47 +0100

    Improve setup

--- setup.py
+++ setup.py
@@ -5,25 +5,43 @@ import ast
 from os import path
 from setuptools import setup
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+PACKAGE_NAME = "pythonwhat"
+REQUIREMENT_NAMES = ["markdown2", "jinja2", "asttokens", "dill", "numpy", "pandas"]
 
-PACKAGE_NAME = 'pythonwhat'
 HERE = path.abspath(path.dirname(__file__))
-with open(path.join(HERE, 'README.md'), encoding='utf-8') as fp:
-    README = fp.read()
-with open(path.join(HERE, PACKAGE_NAME, '__init__.py'), 'rb') as fp:
-    VERSION = str(ast.literal_eval(_version_re.search(
-        fp.read().decode('utf-8')).group(1)))
+VERSION_FILE = path.join(HERE, PACKAGE_NAME, "__init__.py")
+REQUIREMENTS_FILE = path.join(HERE, "requirements.txt")
+README_FILE = path.join(HERE, "README.md")
 
-setup(name=PACKAGE_NAME,
-      version=VERSION,
-      packages=[PACKAGE_NAME, 'pythonwhat.test_funcs'],
-      install_requires=["dill", "numpy", "pandas", "markdown2", "jinja2", "asttokens>=1.1.10"],
-      long_description=README,
-      long_description_content_type='text/markdown',
-      license='GNU version 3',
-      author='DataCamp',
-      author_email='content-engineering@datacamp.com',
-      maintainer='Filip Schouwenaars',
-      maintainer_email='filip@datacamp.com',
-      url='https://github.com/datacamp/pythonwhat')
+with open(VERSION_FILE, encoding="utf-8") as fp:
+    _version_re = re.compile(r"__version__\s+=\s+(.*)")
+    VERSION = str(ast.literal_eval(_version_re.search(fp.read()).group(1)))
+with open(REQUIREMENTS_FILE, encoding="utf-8") as fp:
+    req_txt = fp.read()
+    _requirements_re_template = r"^({}(?:\s*[<>=]+\s*\S*)?)\s*(?:#.*)?$"
+    REQUIREMENTS = [
+        re.search(_requirements_re_template.format(requirement), req_txt, re.M).group(0)
+        for requirement in REQUIREMENT_NAMES
+    ]
+with open(README_FILE, encoding="utf-8") as fp:
+    README = fp.read()
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    packages=[PACKAGE_NAME, "pythonwhat.test_funcs"],
+    install_requires=REQUIREMENTS,
+    description="Submission correctness tests for Python",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Filip Schouwenaars",
+    author_email="filip@datacamp.com",
+    maintainer="Jeroen Hermans",
+    maintainer_email="content-engineering@datacamp.com",
+    url="https://github.com/datacamp/pythonwhat",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+)
