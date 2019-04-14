7131dd284b1dcda2208c844d2d7e0b0a0a5063fb
Jeroen Hermans <jeroen@datacamp.com>
2019-04-14 06:24:52 +0200

    Use updated Feedback interface

--- pythonwhat/Feedback.py
+++ pythonwhat/Feedback.py
@@ -2,7 +2,7 @@ from protowhat.Feedback import Feedback as ProtoFeedback
 
 
 class Feedback(ProtoFeedback):
-    def _line_info(self):
+    def _highlight_data(self):
         if hasattr(self.highlight, "first_token") and hasattr(
             self.highlight, "last_token"
         ):
@@ -13,8 +13,8 @@ class Feedback(ProtoFeedback):
                 "column_end": self.highlight.last_token.end[1],
             }
 
-    def get_formatted_line_info(self):
-        formatted_info = self.get_line_info()
+    def get_highlight_info(self):
+        formatted_info = self.get_highlight_data()
         for k in ["column_start"]:
             if k in formatted_info:
                 formatted_info[k] += 1
