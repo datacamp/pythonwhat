1a3527690cb85c6d398611205f5b6abb441a5594
Jeroen Hermans <jeroen@datacamp.com>
2019-05-09 01:25:15 +0200

    Use numpy in equality test to reduce to boolean

--- pythonwhat/Test.py
+++ pythonwhat/Test.py
@@ -96,7 +96,7 @@ class EqualTest(Test):
         """
         Perform the actual test. result is set to False if the objects differ, True otherwise.
         """
-        self.result = self.func(self.obj1, self.obj2)
+        self.result = np.all(self.func(self.obj1, self.obj2))
 
 
 # Helpers for testing equality
